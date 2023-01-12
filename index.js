c6b4118355d6f3b205bd581a0e5bf28fdda083fb
henry <ihenvyr@gmail.com>
2023-01-12 18:41:03 +0800

    Hashtag and User endpoint update.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fetch = require("node-fetch");
 const express = require("express");
 const puppeteer = require("puppeteer");
 const app = express();
@@ -5,13 +6,34 @@ const app = express();
 (async () => {
   const browser = await puppeteer.launch({headless: true});
 
+  app.get("/hashtag/:hashtag/:period?", async (req, res) => {
+    const { hashtag, period } = req.params;
+
+    const url = `https://ads.tiktok.com/business/creativecenter/hashtag/${hashtag}/pc/en?period=${period || 7}`;
+    const json = await fetch(url)
+      .then(res => res.text())
+      .then(res => JSON.parse(res.match(/{"props":.+?(?=<\/script>)/)[0]));
+
+    res.json(json);
+  });
+
   app.get("/user/:username", async (req, res) => {
     const { username } = req.params;
     const page = await browser.newPage();
+    await page.setJavaScriptEnabled(false);
+
     await page.goto(`https://www.tiktok.com/@${username}`);
 
-    const extractedText = await page.$eval('*', (el) => el.innerHTML);
-    const json = JSON.parse(extractedText.match(/{"AppContext.+?(?=<\/script>)/)[0]);
+    const html = await page.$eval('*', (el) => el.innerHTML);
+    const json = JSON.parse(html.match(/{"AppContext.+?(?=<\/script>)/)[0]);
+
+    delete json["AppContext"];
+    delete json["BizContext"];
+    delete json["SEO"];
+    delete json["SharingMeta"];
+    delete json["UserPage"];
+    delete json["RecommendUserList"];
+
     await page.close();
 
     res.json(json);
