9b52fa9a51a1eb2b86906e589d4b41f6094b4942
henry <ihenvyr@gmail.com>
2023-01-13 18:50:33 +0800

    Json response update.

--- index.js
+++ index.js
@@ -9,17 +9,53 @@ const app = express();
   app.get("/hashtag/:hashtag/:period?", async (req, res) => {
     const { hashtag, period } = req.params;
 
-    const url = `https://ads.tiktok.com/business/creativecenter/hashtag/${hashtag}/pc/en?period=${period || 7}`;
-    const json = await fetch(url)
+    // hashtags insights data
+    const url1 = `https://ads.tiktok.com/business/creativecenter/hashtag/${hashtag}/pc/en?period=${period || 7}`;
+    const json1 = await fetch(url1)
       .then(res => res.text())
       .then(res => JSON.parse(res.match(/{"props":.+?(?=<\/script>)/)[0]));
 
-    res.json(json);
+    // top 3 of trending creators
+    const url2 = `https://ads.tiktok.com/creative_radar_api/v1/popular_trend/hashtag/creator?hashtag=${hashtag}`;
+    const json2 = await fetch(url2)
+      .then(res => res.json());
+
+    res.json({
+      ...json1?.props?.pageProps?.data || {},
+      trendingCreators: json2?.data?.creators || [],
+    });
+  });
+
+  app.get("/videos/:hashtag", async (req, res) => {
+    const { hashtag } = req.params;
+    const page = await browser.newPage(); // open new tab
+    await page.setJavaScriptEnabled(false);
+
+    await page.goto(`https://www.tiktok.com/tag/${hashtag}`);
+
+    const html = await page.$eval('*', (el) => el.innerHTML);
+    const json = JSON.parse(html.match(/{"AppContext.+?(?=<\/script>)/)[0]);
+
+    // remove unnecessary data
+    delete json["AppContext"];
+    delete json["BizContext"];
+    delete json["SEO"];
+    delete json["SharingMeta"];
+    delete json["ItemList"];
+    delete json["ChallengePage"];
+
+    await page.close(); // close tab
+
+    res.json({
+      recentVideos: json?.ItemModule || {},
+      users: json?.UserModule?.users || {},
+      stats: json?.UserModule?.stats || {},
+    });
   });
 
   app.get("/user/:username", async (req, res) => {
     const { username } = req.params;
-    const page = await browser.newPage();
+    const page = await browser.newPage(); // open new tab
     await page.setJavaScriptEnabled(false);
 
     await page.goto(`https://www.tiktok.com/@${username}`);
@@ -27,16 +63,22 @@ const app = express();
     const html = await page.$eval('*', (el) => el.innerHTML);
     const json = JSON.parse(html.match(/{"AppContext.+?(?=<\/script>)/)[0]);
 
+    // remove unnecessary data
     delete json["AppContext"];
     delete json["BizContext"];
     delete json["SEO"];
     delete json["SharingMeta"];
+    delete json["ItemList"];
     delete json["UserPage"];
     delete json["RecommendUserList"];
 
-    await page.close();
+    await page.close(); // close tab
 
-    res.json(json);
+    res.json({
+      ...json?.UserModule?.users?.[username] || {},
+      stats: json?.UserModule?.stats?.[username] || {},
+      recentVideos: json?.ItemModule || {},
+    });
   });
 
   app.listen(3000, () => console.log("ok"));
